55994432efd833b2bc697a12ee1d962bd7fd6862
Barry <barrym07@gmail.com>
2021-10-22 12:23:59 -0600

    student search works

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express();
 const students = require('./students.json');
 
+app.use(express.json());
+
 const port = 3000;
 app.listen(port, () => console.log(`Students app listening at http://localhost:${port}`));
 
-app.get('/students', (req, res) => res.send(students));
 
 
+app.get('/students', (req, res) => {
+  // GET /students - returns a list of all students
+  // this endpoint, optionally, accepts query parameters
+  // GET /students?search=<query> - returns a list of students filtered on name matching the given query
+  if (req.query.search) {
+    let name = decodeURIComponent(req.query.search);
+    let result = students.filter(student => student.name.includes(name))
+    res.send(result)
+  } else {
+    res.send(students)
+  }
+});
