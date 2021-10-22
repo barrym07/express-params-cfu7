d6f9c00d1ff41aa1dbd9a30c9606016564843d99
Barry <barrym07@gmail.com>
2021-10-22 11:00:16 -0600

    /students added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
+const students = require('./students.json');
 
 const port = 3000;
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Students app listening at http://localhost:${port}`));
+
+app.get('/students', (req, res) => res.send(students));
 
 
